3d0f42546911ab231205df22b328afaf8baae20c
innazabu <inna.zabu09@gmail.com>
2024-08-27 20:28:16 +0300

    Update module_3_hard.py

--- module_3_hard.py
+++ module_3_hard.py
@@ -1,68 +1,17 @@
-def calculate_structure_sum(*args):
+def calculate_structure_sum(data):
     sum_elements = 0
-    print(args)
-    data = args[0]
-    for i in data:
-        print(i)
-        first = i
-        if isinstance(i, str):
-            sum_elements += len(i)
-        if isinstance(first, list):
-            for i in first:
-                if isinstance(i, str):
-                    sum_elements += len(i)
-                elif isinstance(i, int):
-                    sum_elements += i
-
-        if isinstance(first, dict):
-            for item in first.values():
-                sum_elements += item
-            for item in first.keys():
-                sum_elements += len(item)
-
-        if isinstance(first, tuple):
-            for item in first:
-                print(item)
-                if isinstance(item, tuple):
-                    if len(item) == 0:
-                        continue
-                if isinstance(item, int):
-                    sum_elements += item
-                if isinstance(item, dict):
-                    for i in item.values():
-                        sum_elements += i
-                    for i in item.keys():
-                        sum_elements += len(i)
-                if isinstance(item, list):
-                    for i in item:
-                        print(i)
-                        if isinstance(i, str):
-                            sum_elements += len(i)
-                        if isinstance(i, int):
-                            sum_elements += i
-                        if isinstance(i, dict):
-                            print(i)
-                            i = i[0]
-                            print('            ', i)
-                            if isinstance(i, tuple):
-                                for j in i:
-                                    if isinstance(j, int):
-                                        sum_elements += j
-                                    if isinstance(j, str):
-                                        sum_elements += len(j)
-
-
-
+    if isinstance(data, int):
+        return data
+    elif isinstance(data, str):
+        return len(data)
+    elif isinstance(data, (list, tuple, set)):
+        for element in data:
+            sum_elements += calculate_structure_sum(element)
+    elif isinstance(data, dict):
+        for key, value in data.items():
+            sum_elements += calculate_structure_sum(key)
+            sum_elements += calculate_structure_sum(value)
     return sum_elements
-
-
-
-
-
-
-
-
-
 
 
 data_structure = [
